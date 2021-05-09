f65933fe253e15bf77c0ffd1fbb19dc8a85a71d4
hardikchhajar <73778599+hardikchhajar@users.noreply.github.com>
2021-05-09 11:01:52 +0530

    Add files via upload

---
+++ bot.php
@@ -0,0 +1,88 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="images/bot.png">
+
+  <title>HealthCare Chatbot</title>
+  
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,600,700,800,900|Ubuntu" rel="stylesheet">
+
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+
+  <!-- Bootstrap core CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+  <!-- Custom styles for this template -->
+  <link href="css/home.css" rel="stylesheet">
+
+</head>
+
+<body class="text-center">
+
+    
+  
+  <div class="cover-container d-flex h-100 p-3 mx-auto flex-column smooth-scroll">
+    <div class="inner container-fluid">
+        <nav class="navbar navbar-expand-lg navbar-default navbar-dark">
+
+          <a style="font-size:4vw;"class="navbar-brand" href=""><img class="chatbot" src="images/robot.png" width="60" height="60" class="d-inline-block align-top" alt="">Chatbot</a>
+
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <ul class="navbar-nav ml-auto">
+                <div id="Buttons", class="Buttons">
+                    <button class="btn btn-light btn-lg" type="button" name="button"><a href="index.php?logout='1'" style="color:black;text-decoration: none;padding:1rem;">Logout</a></button>
+                </div>
+            </ul>
+          </div>
+        </nav>
+        <div>
+            <h1 style="font-size:7vw;" id="title" class="cover-heading">Chatbots</h1>
+            <p style="font-size:1.7vw;" class="lead">Artificial intelligence in healthcare, known also as Deep Medicine, is an overarching term used to describe the use of machine-learning algorithms and software, or artificial intelligence (AI), to mimic human cognition in the analysis, presentation, and comprehension of complex medical and health care data. Specifically, AI is the ability of computer algorithms to approximate conclusions based solely on input data.</p>
+        </div>
+        <div id="Buttons", class="Buttons">
+            <button class="btn btn-light btn-lg" type="button" name="button"><a href="chat.php" style="color:black;text-decoration: none;padding:1rem;">Chatbot</a></button>
+        </div>
+        <div id="Buttons", class="Buttons">
+            <button class="btn btn-light btn-lg" type="button" name="button"><a href="../HCM/chatbot/index.html" style="color:black;text-decoration: none;padding:1rem;">Healthbot</a></button>
+        </div>
+        
+        </div>
+
+        <section id="top">
+
+          <p class="lead">
+            <div class="row">
+            </div>
+
+
+          </p>
+        </section>
+    </div> 
+    <header class="masthead mb-auto">
+    </header>
+
+  </div>
+  <!-- <div>
+    <section></section>
+  </div> -->
+  
+  
+  
+    
+
+</body>
+</html>
